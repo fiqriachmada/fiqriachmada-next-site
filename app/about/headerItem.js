b2400ddaeee99197be165758d636fb8f634669ed
fiqriachmada <achmada1998@gmail.com>
2023-05-31 19:39:38 +0700

    try next js

---
+++ app/about/headerItem.js
@@ -0,0 +1,15 @@
+const headerItems = [
+  {
+    id: 0,
+    title: 'About',
+    link: '/about',
+    //
+  },
+//   {
+//     id: 1,
+//     title: 'Projects',
+//     //
+//   },
+];
+
+export default headerItems;
---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <main>
+      <div className="container">
+        <div className='flex justify-center' >
+          <div>About</div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default About;
